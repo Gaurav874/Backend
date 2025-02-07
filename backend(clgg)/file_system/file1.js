c083f4584193a62deeac1f6148c887247e6c168a
Gaurav <gk1500874@gmail.com>
2025-02-07 09:48:26 +0530

    change somethings

--- backend(clgg)/file_system/file1.js
+++ backend(clgg)/file_system/file1.js
@@ -1,3 +1,13 @@
+// const fs = require('fs');
+// const data = fs.readFileSync("./data.txt", "utf-8");
+// console.log(data.toString());
+
 const fs = require('fs');
-const data = fs.readFileSync("./data.txt", "utf-8");
-console.log(data.toString());
+const data = fs.readFileSync("./data.txt","utf-8");
+console.log(data)
+if(data.match("H")){
+    console.log("H is found");
+    const newdata = data.replace("H","ABESA");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+    
+}
